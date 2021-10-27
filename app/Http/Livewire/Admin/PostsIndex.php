db513b5d240e7458cb8150b350a9d91d1c8d18da
Jesus Cenobio <sistemas@cescorp-americas.com>
2021-10-27 17:14:24 -0500

    subiendo los primeros archivos

---
+++ app/Http/Livewire/Admin/PostsIndex.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+use App\Models\Post;
+
+class PostsIndex extends Component
+{
+    public function render()
+    {
+        $posts = Post::where('user_id', auth()->user()->id)->paginate();
+
+        return view('livewire.admin.posts-index', compact('post'));
+    }
+}
