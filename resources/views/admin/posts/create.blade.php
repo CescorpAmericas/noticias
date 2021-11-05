584ce2a27b0f250d1df3786829e246d3a4558b9e
Jesus Cenobio <sistemas@cescorp-americas.com>
2021-11-05 16:30:42 -0600

    actualizacion 5 del proyecto formulario editar un post

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -7,127 +7,19 @@
 @stop
 
 @section('content')
-<div class="card">
-    <div class="card-body">
-        {!! Form::open(['route' => 'admin.posts.store', 'autocomplete' => 'off', 'files' => true]) !!}
+    <div class="card">
+        <div class="card-body">
+            {!! Form::open(['route' => 'admin.posts.store', 'autocomplete' => 'off', 'files' => true]) !!}
 
-            {!! Form::hidden('user_id', auth()->user()->id) !!}
+                {!! Form::hidden('user_id', auth()->user()->id) !!}
 
-            <div class="form-group">
-                {!! Form::label('name', 'Nombre:') !!}
-                {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el nombre del post']) !!}
-               
-                @error('name')
-                    <small class="text-danger">{{$message}}</small>
-                @enderror
+                @include('admin.posts.partials.form')
 
-            </div>
+                {!! Form::submit('Crear post', ['class' => 'btn btn-primary']) !!}
 
-            <div class="form-group">
-                {!! Form::label('slug', 'Slug:') !!}
-                {!! Form::text('slug', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el slug del post', 'readonly']) !!}
-                
-                @error('slug')
-                <small class="text-danger">{{$message}}</small>
-            @enderror
-
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('category_id', 'Categoría') !!}
-                {!! Form::select('category_id', $categories, null, ['class' => 'form-control']) !!}
-            
-                @error('category_id')
-                    <small class="text-danger">{{$message}}</small>
-                @enderror
-
-            </div>
-
-            <div class="form-group">
-                <p class="font-weight-bold">Etiquetas</p>
-
-                @foreach ($tags as $tag)
-
-                    <label class="mr-2">
-                        {!! Form::checkbox('tags[]', $tag->id, null) !!}
-                        {{$tag->name}}
-                    </label>
-
-                @endforeach
-
-                @error('tags')
-                    <br>
-                    <small class="text-danger">{{$message}}</small>
-                @enderror
-                
-            </div>
-
-            <div class="form-group">
-                <p class="font-weight-bold">Estado</p>
-
-                <label>
-                    {!! Form::radio('status', 1, true) !!}
-                    Borrador
-                </label>
-                <label>
-                    {!! Form::radio('status', 2) !!}
-                    Publicado
-                </label>
-
-                @error('status')
-                <br>
-                    <small class="text-danger">{{$message}}</small>
-                @enderror
-
-            </div>
-
-            <div class="row mb-3">
-                <div class="col">
-                    <div class="image-wrapper">
-                        <img id="picture" src="https://cdn.pixabay.com/photo/2018/05/14/20/54/truck-3401529_960_720.jpg" alt="">
-                    </div>
-                </div>
-
-                <div class="col">
-                    <div class="form-group">
-                        {!! Form::label('file', 'Imagen que se mostrara en el post') !!}
-                        {!! Form::file('file', ['class' => 'form-control-file', 'accept' => 'image/*']) !!}
-                        
-                        @error('file')
-                            <span class="text-danger">{{$message}}</span>
-                        @enderror
-
-                    </div>
-
-                    <p>srffbuiwfiusefiwe</p>
-                </div>
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('extract', 'Extracto:') !!}
-                {!! Form::textarea('extract', null, ['class' => 'form-control']) !!}
-
-                @error('extract')
-                  <small class="text-danger">{{$message}}</small>
-                @enderror
-
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('body', 'Colocar cuerpo del post:') !!}
-                {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
-
-                @error('body')
-                  <small class="text-danger">{{$message}}</small>
-                @enderror
-
-            </div>
-
-            {!! Form::submit('Crear post', ['class' => 'btn btn-primary']) !!}
-
-        {!! Form::close() !!}
+            {!! Form::close() !!}
+        </div>
     </div>
-</div>
 @stop
 
 @section('css')
