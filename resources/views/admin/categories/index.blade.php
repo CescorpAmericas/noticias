8404201329d4ab5962c1133284cd582cc98a6340
Jesus Cenobio <sistemas@cescorp-americas.com>
2021-11-18 16:31:25 -0600

    actualizacion 9 como ocultar botones segun el rol

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -3,6 +3,11 @@
 @section('title', 'CESCORP')
 
 @section('content_header')
+
+    @can('admin.categories.create')
+    <a href="{{route('admin.categories.create')}}" class="btn btn-secondary btn-sm float-right">Agregar categoría</a>
+    @endcan
+
     <h1>Lista de categorías</h1>
 @stop
 
@@ -15,10 +20,6 @@
     @endif
 
     <div class="card">
-
-        <div class="card-header">
-            <a href="{{route('admin.categories.create')}}" class="btn btn-secondary">Agregar categoría</a>
-        </div>
 
         <div class="card-body">
             <table class="table table-striped">
@@ -35,15 +36,20 @@
                             <td>{{$category->id}}</td>
                             <td>{{$category->name}}</td>
                             <td width="10px">
+                                @can('admin.categories.edit')
                                 <a href="{{route('admin.categories.edit', $category)}}" class="btn btn-primary btn-sm">Editar</a>
+                                @endcan
                             </td>
                             <td width="10px">
-                                <form action="{{route('admin.categories.destroy', $category)}}" method="POST">
-                                    @csrf
-                                    @method('delete')
+                                @can('admin.categories.destroy')
+                                    
+                                    <form action="{{route('admin.categories.destroy', $category)}}" method="POST">
+                                        @csrf
+                                        @method('delete')
 
-                                    <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
-                                </form>
+                                        <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
+                                    </form>
+                                @endcan
                             </td>
                         </tr>
                     @endforeach
